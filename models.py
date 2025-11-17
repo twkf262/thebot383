d00b3e5ecbcc16c0307632e858b8ed480e3cfaa4
twkf262 <fueb38341@proton.me>
2025-11-17 19:56:45 +0000

    Create models.py

---
+++ models.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    telegram_id = Column(String, unique=True, index=True)
+    name = Column(String)
+    age = Column(Integer)
